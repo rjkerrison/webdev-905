2ade76d9d4682d040f633d7fa2fe17a80ac5bf81
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-05-10 10:51:22 +0200

    functions lesson walkthrough

--- w1/d2/functions-intro.js
+++ w1/d2/functions-intro.js
@@ -12,13 +12,21 @@ A function is for:
 
 */
 
+// sayHello() will error here because className is not yet defined
+const className = 'WebDev 905'
+// this is okay – sayHello is hoisted
+sayHello()
 // function definition / function declaration / function statement
-function sayHello() {
-  console.log('Hello!')
-}
 
+// function keyword followed by the name followed by brackets containing (empty) parameters
+// followed by a code block (code wrapped in {})
+function sayHello() {
+  console.log(`Hello, ${className}!`)
+}
 sayHello()
-sayHello()
+
+// calling the function
+// name of the function followed by brackets (parentheses)
 
 // function expression
 const sayExpression = function () {
@@ -26,6 +34,7 @@ const sayExpression = function () {
 }
 sayExpression()
 
+// function expressions can allow us to change to which function a variable name points
 let sayGreeting = function () {
   console.log('Bonjour')
 }
@@ -42,37 +51,51 @@ const sayAreet = () => {
 }
 sayAreet()
 
-/*
-Let's look at these.
+// /*
+// Let's look at these.
 
-- Accept some data ("arguments", "parameters")
-- Can return / give back some data ("return value of the function")
-*/
+// - Accept some data ("arguments", "parameters")
+// - Can return / give back some data ("return value of the function")
+// */
 
 // parameters and arguments are assigned by order
 function multiply(a, b) {
-  // a and b are the parameter
-  console.log(arguments)
+  console.log('started the function')
+
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    console.log('Error: not numbers!', a, b)
+    // this return is conditional – it won't always be hit
+    return NaN
+  }
+
+  // a and b are the parameters
   return a * b
+  // won't be executed - it's after the return!
+  console.log('finished the function')
 }
-// 24 and 26 are the arguments
-console.log(multiply(24, 26, 'another argument?'))
+// 24 and 26 are the arguments,
+// so this function call becomes 624 when we log it
+console.log(multiply(24, 26))
+const bigNumber = multiply(729, 1024)
+console.log(bigNumber)
+multiply('Robin', 'Paddington')
 
 function playFizzbuzz(number) {
   // when number is divisible by 3, say "fizz"
   // when number is divisible by 5, say "buzz"
   // when number is divisible by 15, say "fizzbuzz"
   // otherwise say the number
+  let result = number
+
   if (number % 15 === 0) {
-    return 'fizzbuzz'
-  }
-  if (number % 3 === 0) {
-    return 'fizz'
+    result = 'fizzbuzz'
+  } else if (number % 3 === 0) {
+    result = 'fizz'
   } else if (number % 5 === 0) {
-    return 'buzz'
+    result = 'buzz'
   }
-  // console.log(number, 'line 69')
-  return number
+
+  return result
 }
 
 for (let i = 1; i < 20; i++) {
@@ -89,7 +112,16 @@ function calculateAnnualBudget() {
 }
 
 // TODO - fill these later
-function calculateAnnualCosts() {}
+function calculateAnnualCosts() {
+  const costsForBuildingMaintenance = getBuildingMaintenanceCosts()
+  return costsForBuildingMaintenance
+}
 const calculateKnownIncome = () => {}
 const findSpareMoney = () => {}
 const saveToDatabase = () => {}
+
+function getBuildingMaintenanceCosts() {}
+
+function noReturnValue() {}
+
+console.log('noReturnValue returns:', noReturnValue())
