a21f474896bacc7f3f28a324229863d31d68a239
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-05-17 12:25:52 +0200

    event listener demo

--- w2/d2/demo.js
+++ w2/d2/demo.js
@@ -3,14 +3,45 @@ console.log('I ADDED EVER MORE JS!')
 
 document.querySelector('h1').textContent = 'manipulated by JS'
 
-// using JS objects to represent new DOM elements
-const createdParagraph = document.createElement('p')
-// make changes like textContent
-createdParagraph.textContent = 'I was created by JS'
-// set classes
-createdParagraph.classList.add('special', 'hello')
+function addParagraph(content = 'I was created by JS') {
+  // using JS objects to represent new DOM elements
+  const createdParagraph = document.createElement('p')
+  // make changes like textContent
+  createdParagraph.textContent = content
+  // set classes
+  createdParagraph.classList.add('special', 'hello')
+  console.log(createdParagraph)
 
-console.log(createdParagraph)
+  // add the created element into the page
+  document.body.appendChild(createdParagraph)
+}
 
-// add the created element into the page
-document.body.appendChild(createdParagraph)
+// when the user clicks on the button,
+// we add a paragraph with the user-provided value from the input
+
+// We will need:
+// - BUTTON
+const button = document.querySelector('button#add-message')
+// - INPUT
+const messageInput = document.querySelector('input#message')
+
+// always check your variables are what you think they are
+console.log(button, messageInput)
+
+// We will need to:
+// - get the input value : getInputValue()
+function getInputValue() {
+  return messageInput.value
+}
+// - add the paragraph : addParagraph()
+// - listen for the button click
+
+// element.addEventListener(eventType, callback)
+button.addEventListener('click', (event) => {
+  console.log('EVENT TARGET', event.target.id)
+
+  // first test the listener is correct
+  console.log('YOU CLICKED THE BUTTON')
+  const value = getInputValue()
+  addParagraph(value)
+})
--- w2/d2/main.js
+++ w2/d2/main.js
@@ -4,13 +4,19 @@ const redButton = document.getElementById('red')
 const yellowButton = document.getElementById('yellow')
 const greenButton = document.getElementById('green')
 const resultElement = document.querySelector('p#result')
+const heading = document.querySelector('h1')
+
+console.log('heading', heading)
 
 console.log('red', redButton)
 console.log('yellow', yellowButton)
 console.log('green', greenButton)
 
-// EFFECT (side effect/function/thing we want to happen)
+// EFFECT/EXECUTION (side effect/function/thing we want to happen)
 const changeText = (event) => {
+  console.log('EVENT TARGET DATA CLASS', event.target.dataset.class)
+  console.log('EVENT TARGET DATA HINT', event.target.dataset.hint)
+
   const newText = event.target.getAttribute('data-hint')
   const newClass = event.target.getAttribute('data-class')
   // Set the text content of that element to the hint
