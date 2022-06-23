2f66faa627724a8964449b2989ab3f3816defb43
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-06-23 17:37:48 +0200

    setSearchParams example

--- w7/d4/full-stack-app/src/pages/About.jsx
+++ w7/d4/full-stack-app/src/pages/About.jsx
@@ -4,13 +4,12 @@ import { useSearchParams } from 'react-router-dom'
 const About = ({ setName, name }) => {
   const [searchParams, setSearchParams] = useSearchParams()
 
-  console.log(Array.from(searchParams.entries()))
+  const queryParams = Object.fromEntries(searchParams.entries())
 
   return (
     <div>
       <h2>About</h2>
-      <p>searchParams are {JSON.stringify(searchParams)}</p>
-      <p>q is {searchParams.get('q')}</p>
+      <p>searchParams are {JSON.stringify(queryParams)}</p>
       <input
         type='text'
         name=''
@@ -18,6 +17,19 @@ const About = ({ setName, name }) => {
         value={name}
         onChange={(e) => setName(e.target.value)}
       />
+      <div>
+        <button
+          onClick={() =>
+            setSearchParams({
+              ...queryParams,
+              name,
+              number: Math.random() * 100,
+            })
+          }
+        >
+          Add some params
+        </button>
+      </div>
     </div>
   )
 }
